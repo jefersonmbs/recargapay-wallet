a309d6e351733d2a757e5b6fc00fe83c235ed89e
Jeferson <jeferson@lailla.io>
2025-08-26 20:36:15 -0300

    Initial commit

---
+++ src/main/java/br/com/jefersonmbs/recargapaywallet/RecargapayWalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.jefersonmbs.recargapaywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecargapayWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecargapayWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/jefersonmbs/recargapaywallet/RecargapayWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jefersonmbs.recargapaywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecargapayWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
